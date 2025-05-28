3f9f1469a67b45dd8a5a6074c966f674da0ac38e
Nahuel <nahuelaba96@gmail.com>
2025-05-28 16:54:28 +0000

    agrego conexion a db en atlas mongo

--- database/mongo.go
+++ database/mongo.go
@@ -13,11 +13,21 @@ import (
 var MongoClient *mongo.Client
 var VehiculosCollection *mongo.Collection
 
+// mongo password RMc2axKooRJ5hGrT
+// mongo username nahuelaba96
+
 func ConnectMongo() {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
-	clientOptions := options.Client().ApplyURI("mongodb://mongo:27017")
+	// compose
+	//clientOptions := options.Client().ApplyURI("mongodb://mongo:27017")
+
+	// local
+	//clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+
+	// mongo
+	clientOptions := options.Client().ApplyURI("mongodb+srv://nahuelaba96:RMc2axKooRJ5hGrT@cluster-gestion-vehicul.czzbkqo.mongodb.net/?retryWrites=true&w=majority&appName=cluster-gestion-vehicular")
 	client, err := mongo.Connect(ctx, clientOptions)
 	if err != nil {
 		log.Fatal("Error al conectar con MongoDB:", err)
--- go.mod
+++ go.mod
@@ -18,7 +18,7 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.26.0 // indirect
 	github.com/goccy/go-json v0.10.5 // indirect
-	github.com/golang/snappy v0.0.4 // indirect
+	github.com/golang/snappy v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/compress v1.16.7 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.10 // indirect
@@ -35,6 +35,7 @@ require (
 	github.com/xdg-go/stringprep v1.0.4 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20240726163527-a2c0da244d78 // indirect
 	go.mongodb.org/mongo-driver v1.17.3 // indirect
+	go.mongodb.org/mongo-driver/v2 v2.2.1 // indirect
 	golang.org/x/arch v0.15.0 // indirect
 	golang.org/x/crypto v0.36.0 // indirect
 	golang.org/x/net v0.38.0 // indirect
