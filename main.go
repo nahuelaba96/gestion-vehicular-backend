cfc796fedce6fc3f4557d6ae76764a469ee82d42
Nahuel <nahuelaba96@gmail.com>
2025-06-04 12:28:14 +0000

    agrego cors

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ func main() {
 	database.ConnectMongo()
 
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173", "http://127.0.0.1:5173"},
+		AllowOrigins:     []string{"http://localhost:5173", "http://127.0.0.1:5173","https://c196-45-234-34-114.ngrok-free.app",},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
